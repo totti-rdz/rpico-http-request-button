ee211992e28b52de7c72d137145f5b089f886510
totti-rdz <thorsten_herfurtner@gmx.de>
2024-09-30 03:27:29 +0200

    feat: make HTTP request on button press

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import wifi
+from picozero import Button
+import urequests
 
-from secrets import SSID, PASSWORD
+from secrets import PASSWORD, SSID, URL
+
+def createRequest(url):
+    def request():
+        print("button pressed")
+        response = urequests.get(url)
+        response.close()
+    return request
+
+button = Button(14)
 
 try:
     ip = wifi.connect(SSID, PASSWORD)
+    button.when_pressed = createRequest(URL)
 
 except KeyboardInterrupt:
     machine.reset()
--- secrets.template.py
+++ secrets.template.py
@@ -1,2 +1,4 @@
 SSID = 'Your_WIFI_Name'
 PASSWORD = 'Your_WIFI_Password'
+
+URL = 'http://...'
