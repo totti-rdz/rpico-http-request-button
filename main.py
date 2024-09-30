9c9deb35b1be9e3491bec695ab07b7eb63f2c881
totti-rdz <thorsten_herfurtner@gmx.de>
2024-09-30 02:54:36 +0200

    feat: extract wifi connection into module

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import network
-from time import sleep
+import wifi
 
 from secrets import SSID, PASSWORD
 
-#Connect to WLAN
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(SSID, PASSWORD)
-while wlan.isconnected() == False:
-    print('Waiting for connection...')
-    sleep(1)
-ip = wlan.ifconfig()[0]
-print(f'Connected on {ip}')
+ip = wifi.connect(SSID, PASSWORD)
